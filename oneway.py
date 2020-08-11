6b1482bba3fd9583c3bde4765bd7f355cb6c5b53
lmcnamar <lauren.mcnamara@gmail.com>
2020-08-11 14:01:59 -0500

    sort mixed numeric and alpha values, ascending or descending

--- oneway.py
+++ oneway.py
@@ -12,27 +12,38 @@ class oneway:
         """
         print(self.name)
 
-    def freqtable(self, sort='value', ): 
+    def freqtable(self, sort='value', asc=True): 
         """
         One-way frequency table.
 
         sort: value (default), number, or pct
+        asc: sort ascending (True/False)
         """
 
+        ### summing
         table = (pd.concat([self.series.value_counts().rename('count'), 
                 self.series.value_counts(normalize=True).mul(100).rename('percentage')], 
                 axis=1)
                 .reset_index()
                 .rename(columns={'index': 'value'}))
         
+        ### sorting
         if sort in ['value','count','pct','percentage']:
             if sort == 'pct':
                 sort = 'percentage'
-            
-            table = table.sort_values(by=[sort])
+
+            if sort == 'value':
+                table = table.loc[pd.to_numeric(table.value, errors='coerce')   \
+                                    .sort_values(na_position = 'first'
+                                                ,ascending = asc)  \
+                                    .index]
+            else:
+                table = table.sort_values(by=[sort]
+                                        ,ascending = asc)
         else: 
             print('Invalid sort!')
  
+        ### cumulating
         totn = table['count'].sum()
 
         table = (pd.concat([table,
